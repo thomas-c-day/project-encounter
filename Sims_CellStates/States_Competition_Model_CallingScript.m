5c28ab98e858280730247007c5f502dd7c77056f
Thomas C. Day <45134242+thomas-c-day@users.noreply.github.com>
2025-07-23 14:42:35 -0700

    Update and rename States_Competition_Model_CallingScript.m to Sims_CellStates/States_Competition_Model_CallingScript.m

--- Sims_CellStates/States_Competition_Model_CallingScript.m
+++ Sims_CellStates/States_Competition_Model_CallingScript.m
@@ -4,9 +4,10 @@
 % group out-competing the smaller group even when disadvantaged by mean.
 
 % INPUTS ------------------------------------------------------------------
-Msims   = 1;                          % number of simulations to run
+Msims   = 1;                            % number of simulations to run
 FigViz  = 2;                            % T/F show the figures of the simulations
 Nrounds = 2e2;                          % number of rounds of selection
+Simple_Random_Switch = 2;               % switch that choose whether to do a simulation that has no randomness (1), or has randomness/fluctuations (2 or any other number)
 
 % Compile traits for the two competing strategies:
 alpha   = 2.5;                          % scaling exponent for encounter rate vs. size
@@ -14,7 +15,7 @@ phi     = 0.3;                          % packing fraction to turn trait -> size
 G_0     = 0.01;                         % encounter rate for the resident strategy [# encounters/doubling time]
 delta   = 3e-2;                         % decay rate [#/doubling time]
 Kappa   = 1e5;                          % carrying capacity of the population
-Mu      = [1; 1];                       % maximum growth rate of the two strategies [# divisions/doubling time]
+Mu      = [1; 1];                       % maximum growth rate of the two strategies [# divisions/doubling time] (left S1 singled celled, right S2 or multi-celled)
 K_m     = [0.5; 0.5];                   % half-max number of encounters/capita to get max growth rate, Monod dynamics
 Delta   = [-delta; -delta];             % death rates for each strategy, constant
 DeathThresh  = [0.90; 0.90];            % the biomass threshold at which an individual dies
@@ -25,7 +26,7 @@ DivideThresh = [2.00; Inf];             % the biomass threshold at which an indi
 for mm = 1:Msims
     
     % Run a simulation with fluctuations:
-    [n_frac, b_frac, ~, ~, NumInd] = RUN_SINGLE_SIMULATION_STATES(Nrounds, G_0, alpha, phi, 0, Mu, K_m, Delta, Kappa, DivideThresh, DeathThresh, FigViz);
+    [n_frac, b_frac, ~, ~, NumInd] = RUN_SINGLE_SIMULATION_STATES(Nrounds, G_0, alpha, phi, 0, Mu, K_m, Delta, Kappa, DivideThresh, DeathThresh, FigViz, Simple_Random_Switch);
         % n_track: the population list, where each entry corresponds to a
             % member of the population. 1=resident, 2=mutant.
         % b_track: biomass list, each entry corresponds to one member of
